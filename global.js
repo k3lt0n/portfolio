005b556e604a91420479e1e28b0cf128b005f1d1
k3lt0n <kelton@mit.edu>
2025-02-27 17:45:57 -0500

    add js, A2, A3

--- global.js
+++ global.js
@@ -4,9 +4,62 @@ function $$ (selector, context = document) {
 	return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
-let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname)
-currentLink.classList.add("current");
-if (currentLink) { // or if (currentLink !== undefined)
-	currentLink.classList.add("current");
+
+let pages = [
+	{url: "", title: "Home"},
+	{url: "projects/", title: "Projects"},
+	{url: "contact/", title: "Contact Me"},
+	{url: "resume/", title: "Resume"},
+	{url: "https://github.com/k3lt0n", title: "GitHub"},
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
+for (let p of pages) {
+	let url = p.url;
+	if (!ARE_WE_HOME && !url.startsWith("http")) {
+		url = "../" + url;
+	}
+	let title = p.title; // Create link and add it to nav
+	let a = document.createElement("a");
+	a.href = url;
+	a.textContent = title;
+	if (a.host === location.host && a.pathname === location.pathname) {
+		a.classList.add("current");
+	}
+	if (a.host !== location.host) {
+		a.target = "_blank";
+	}
+	nav.append(a);
 }
+
+document.body.insertAdjacentHTML("afterbegin", `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option value ="light dark"> Automatic </option>
+			<option value = "dark"> Dark Mode </option>
+			<option value = "light"> Light Mode </option>
+		</select>
+	</label>`
+);
+
+let select = document.querySelector("select");
+select.addEventListener("input", function (event) {
+	console.log("color scheme changed to", event.target.value);
+	document.documentElement.style.setProperty("color-scheme", event.target.value);
+	localStorage.colorScheme = event.target.value;
+});
+
+if (localStorage.colorScheme) {
+	document.documentElement.style.setProperty("color-scheme", localStorage.colorScheme);
+	select.value = localStorage.colorScheme;
+}
+
+
+
+
+
